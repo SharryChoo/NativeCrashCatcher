4f78ee7543e0b0887d815fdf1f2f83fc8f359d85
sharry <zhuxiaoyu.sharry@bytedance.com>
2020-09-01 00:56:24 +0800

    feature(catcher): 添加了 Native crash 捕获的框架代码

--- lib-native-crash-catcher/src/main/cpp/SampleCrashCatcher.h
+++ lib-native-crash-catcher/src/main/cpp/SampleCrashCatcher.h
@@ -6,23 +6,35 @@
 #define NATIVECRASHCATCHER_SAMPLECRASHCATCHER_H
 
 
+struct CrashContext {
+    siginfo_t siginfo;
+    pid_t tid;  // the crashing thread.
+    ucontext_t context;
+#if !defined(__ARM_EABI__) && !defined(__mips__)
+    // #ifdef this out because FP state is not part of user ABI for Linux ARM.
+    // In case of MIPS Linux FP state is already part of ucontext_t so
+    // 'float_state' is not required.
+    fpstate_t float_state;
+#endif
+};
+
 class SampleCrashCatcher {
 
 public:
     SampleCrashCatcher();
     ~SampleCrashCatcher();
 
-private:
-    bool hasInit = false;
-    // 替换系统的信号处理栈
-    void installAlterStack();
-    // 恢复系统信号处理栈
-    void restoreAlterStack();
-
-    // 替换系统信号处理函数
-    void installSysSigHandler();
+    // 重置成为内核默认的信号处理器
+    static void restoreKernelDefaultSigHandler(int sig);
     // 恢复系统信号处理函数
-    void restoreSysSinHandler();
+    static void restoreSysSinHandler();
+    // 替换系统的信号处理栈
+    static void installAlterStack();
+    // 恢复系统信号处理栈
+    static void restoreAlterStack();
+    // 替换系统信号处理函数
+    static void installSysSigHandler();
+
 };
 
 
